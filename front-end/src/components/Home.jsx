b208d125f1416cc8f032f3dc081f409cba53e33e
rafiqhere <rafiqulislamhere@gmail.com>
2026-05-06 02:33:42 +0600

    27th commit

--- front-end/src/components/Home.jsx
+++ front-end/src/components/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
         <div>
             <h3>This is home.......</h3>
             <h2>I'm in the home !!!</h2>
+            <h1>Working on it.</h1>
             <Users></Users>
 
         </div>
