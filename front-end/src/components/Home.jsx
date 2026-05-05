ad9e023723eaadfb622737c0960ba419759be8c2
rafiqhere <rafiqulislamhere@gmail.com>
2026-05-06 02:28:33 +0600

    25th commit

---
+++ front-end/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Users from './Users'
+
+const Home = () => {
+    return (
+        <div>
+            <h3>This is home.......</h3>
+            <h2>I'm in the home !!!</h2>
+            <Users></Users>
+
+        </div>
+    )
+}
+
+export default Home
