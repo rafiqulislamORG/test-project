925e05c949226af11feb0e311a35878475565d1a
rafiqul islam <rafiqulislamhere@gmail.com>
2026-05-06 02:46:42 +0600

    30th commit (#6)

--- front-end/src/components/Home.jsx
+++ front-end/src/components/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
         <div>
             <h3>This is home.......</h3>
             <h2>I'm in the home !!!</h2>
+            <h4>We are here with you to help you</h4>
             <Users></Users>
 
         </div>
